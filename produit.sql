da0c496b0d306600cb246868713e44310af73540
Mehdi28630 <78013620+Mehdi28630@users.noreply.github.com>
2022-06-09 20:32:53 +0200

    Update produit.sql

--- produit.sql
+++ produit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 12 mai 2022 à 14:27
+-- Généré le : mer. 08 juin 2022 à 16:34
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -24,6 +24,44 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `ajoute`
+--
+
+DROP TABLE IF EXISTS `ajoute`;
+CREATE TABLE IF NOT EXISTS `ajoute` (
+  `ID_USER` int(32) NOT NULL,
+  `ID_PRODUIT` int(32) NOT NULL,
+  PRIMARY KEY (`ID_USER`,`ID_PRODUIT`),
+  KEY `I_FK_AJOUTE_USER` (`ID_USER`),
+  KEY `I_FK_AJOUTE_PRODUIT` (`ID_PRODUIT`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+DROP TABLE IF EXISTS `categorie`;
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `ID_CATEGORIE` int(32) NOT NULL,
+  `LIBELLE` varchar(32) NOT NULL,
+  PRIMARY KEY (`ID_CATEGORIE`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `categorie`
+--
+
+INSERT INTO `categorie` (`ID_CATEGORIE`, `LIBELLE`) VALUES
+(1, 'Conserves'),
+(2, 'Apéritifs'),
+(3, 'Produits Ménagers'),
+(4, 'Biscuits');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `produit`
 --
 
@@ -35,14 +73,60 @@ CREATE TABLE IF NOT EXISTS `produit` (
   `QUANTITÉ` int(100) NOT NULL,
   PRIMARY KEY (`ID_PRODUIT`),
   KEY `I_FK_PRODUIT_CATEGORIE` (`ID_CATEGORIE`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `produit`
 --
 
 INSERT INTO `produit` (`ID_PRODUIT`, `ID_CATEGORIE`, `LIBELLE`, `QUANTITÉ`) VALUES
-(1, 1, 'Conserve', 2304);
+(3, 3, 'Savon', 54),
+(4, 4, 'Prince', 212);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE IF NOT EXISTS `role` (
+  `ID_ROLE` int(32) NOT NULL,
+  `LIBELLE` varchar(32) NOT NULL,
+  PRIMARY KEY (`ID_ROLE`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `role`
+--
+
+INSERT INTO `role` (`ID_ROLE`, `LIBELLE`) VALUES
+(1, 'admin'),
+(2, 'client');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `ID_USER` int(32) NOT NULL AUTO_INCREMENT,
+  `ID_ROLE` int(32) NOT NULL,
+  `IDENTIFIANT` varchar(32) NOT NULL,
+  `MOTDEPASSE` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID_USER`),
+  KEY `I_FK_USER_ROLE` (`ID_ROLE`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`ID_USER`, `ID_ROLE`, `IDENTIFIANT`, `MOTDEPASSE`) VALUES
+(3, 2, 'anton', '$2a$11$S2TrVgsz3EcPyO2wvNOY3OaS2y95OZvfYWINoLjCeAv6vmlh1.H.2'),
+(2, 1, 'mehdi', '$2a$11$LL8.TJOcjELev3.umo1/aeDMt/CCpkEasgmPEQ2R.VMjjb1.Tbl2C');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
